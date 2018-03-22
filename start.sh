17fd2120974e6a7081df7275665bddfd0a00f480
isaac <isaacpe@gmail.com>
2018-03-22 16:21:39 +0100

    version 2

--- start.sh
+++ start.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 #when started
+#/home/ubuntu/seat-test/SEAT-test-master
+sudo export FLASK_APP=/home/ubuntu/seat-test/SEAT-test-master/minitwit.py
 sudo killall python
 sudo python -m flask run --host=0.0.0.0 --port=80 &
